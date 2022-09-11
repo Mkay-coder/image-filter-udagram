84ffed44f6c8399c0d0908fbf3b0187178837445
Mkay-coder <79610931+Mkay-coder@users.noreply.github.com>
2022-09-11 13:45:48 +0300

    Step 1- Filter image function added to server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
@@ -28,6 +28,21 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
+
+  app.get('/filteredImage', async (request: Request, response: Response) =>{
+    const  image_url  = request.query.image_url.toString();
+
+    //Validating image_url query
+    if(!image_url){
+      response.status(400).send('An image url is required!');
+    }
+
+    const filteredImage = await filterImageFromURL(image_url);
+
+    response.status(200).sendFile(filteredImage, () =>{
+      deleteLocalFiles([filteredImage])
+    })
+  })
 
   //! END @TODO1
   
